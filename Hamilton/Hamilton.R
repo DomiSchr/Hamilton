8351776d01b793941dfe9f5d1923a907b485b723
DomiSchr <domischroeder@gmx.de>
2020-03-21 19:19:54 +0100

    Besser formatiert

--- Hamilton/Hamilton.R
+++ Hamilton/Hamilton.R
@@ -1,8 +1,8 @@
 #Hamilton-Method for the Apportionment Problem
 
-hamilton <- function(p, H){
-  P <- sum(p2[,"population"])
-  A <- P/H
+hamilton <- function(p, H) {
+  P <- sum(p2[, "population"])
+  A <- P / H
   
   share <- array(0, length(p))
   share_floor <- array(0, length(p))
@@ -12,27 +12,28 @@ hamilton <- function(p, H){
   p[["share_floor"]] <- share_floor
   p[["fraction"]] <- fraction
   
-  for(i in 1:length(p[["population"]])){
-    p[i,"share"] <- p[i,"population"]/A
-    p[i, "share_floor"] <- floor(p[i,"share"])
-    p[i, "fraction"] <-  p[i,"share"] - p[i, "share_floor"]
+  for (i in 1:length(p[["population"]])) {
+    p[i, "share"] <- p[i, "population"] / A
+    p[i, "share_floor"] <- floor(p[i, "share"])
+    p[i, "fraction"] <-  p[i, "share"] - p[i, "share_floor"]
     
   }
-
-  if(sum(p[, "share_floor"]) == H){
-    return(p)
-  } else {
-
-    p[["result"]] <- replicate(1, p[["share_floor"]])
-    
-    ranks <- order(p$fraction, decreasing = TRUE)
-    for(i in 1:(H - sum(p[, "share_floor"]))){
-      p[[ranks[i], "result"]] <-  p[[ranks[i], "result"]] + 1
-    }
+  
+  if (sum(p[, "share_floor"]) == H) {
     return(p)
   }
+  
+  p[["result"]] <- replicate(1, p[["share_floor"]])
+  
+  ranks <- order(p$fraction, decreasing = TRUE)
+  for (i in 1:(H - sum(p[, "share_floor"]))) {
+    p[[ranks[i], "result"]] <-  p[[ranks[i], "result"]] + 1
+  }
+  return(p)
 }
 
-p2 <- data.frame("state"=c("A", "B", "C", "D"), "population" = c(630, 480, 390, 500))
+p2 <-
+  data.frame("state" = c("A", "B", "C", "D"),
+             "population" = c(630, 480, 390, 500))
 
 hamilton(p2, 10)
