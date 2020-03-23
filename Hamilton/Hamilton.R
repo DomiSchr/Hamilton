cb5f4d6ec33e4afe533c9aab50293b02d2940a4d
DomiSchr <domischroeder@gmx.de>
2020-03-23 22:18:10 +0100

    Ein Bug raus, anderer wieder drin!

--- Hamilton/Hamilton.R
+++ Hamilton/Hamilton.R
@@ -2,28 +2,32 @@ Hamilton <- function(p, h) {
   # Implemented Hamilton-Method for the Apportionment Problem
   #
   # Args:
-  #   x: a data frame with the names of the states as first column 
+  #   p: a data frame with the names of the states as first column 
   #     and the equivalent population
-  #     size in the second column named 'population'.
+  #     size in the second column.
   #   h: the house size as variable.
   #
   # Returns:
   #   A DataFrame containing all the Information 
   #   and the final result in the last column.
   
-  psum <- sum(p2[, "population"])
+  psum <- sum(p[, 2])
   a <- psum / h
   
   share <- array(0, length(p))
   share.floor <- array(0, length(p))
   fraction <- array(0, length(p))
   
-  p[["share"]] <- share
-  p[["share.floor"]] <- share.floor
-  p[["fraction"]] <- fraction
+  p[["share"]] <- NA
+  p["share"] <- share
+  browser()
+  p[["share.floor"]] <-NA
+  p["share.floor"] <- share.floor
+  p[["fraction"]] <- NA
+  p["fraction"] <- fraction
   
-  for (i in 1:length(p[["population"]])) {
-    p[i, "share"] <- p[i, "population"] / a
+  for (i in 1:length(p[,2])) {
+    p[i, "share"] <- p[i, 2] / a
     p[i, "share.floor"] <- floor(p[i, "share"])
     p[i, "fraction"] <-  p[i, "share"] - p[i, "share.floor"]
     
@@ -32,8 +36,8 @@ Hamilton <- function(p, h) {
   if (sum(p[, "share.floor"]) == h) {
     return(p)
   }
-  
-  p[["result"]] <- replicate(1, p[["share.floor"]])
+  p[["result"]] <- NA
+  p["result"] <- replicate(1, p[["share.floor"]])
   
   ranks <- order(p$fraction, decreasing = TRUE)
   for (i in 1:(h - sum(p[, "share.floor"]))) {
@@ -41,3 +45,9 @@ Hamilton <- function(p, h) {
   }
   return(p)
 }
+
+states <- c("a","b", "c")
+population <- c(630, 480, 390)
+
+p <- data.frame(states, population )
+Hamilton(p,10)
