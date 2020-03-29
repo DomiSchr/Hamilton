af2523c61580ef4021d009bdc39a3852e7abf149
DomiSchr <domischroeder@gmx.de>
2020-03-29 18:13:03 +0200

    Typos fixed

--- Hamilton/Hamilton.R
+++ Hamilton/Hamilton.R
@@ -1,18 +1,42 @@
-Hamilton <- function(p, h) {
-  # Implemented Hamilton-Method for the Apportionment Problem
+LargestRemainderMethod <- function(p, h, q = 0) {
+  #  Largest remainder method for the Apportionment Problem
   #
   # Args:
   #   p: a data frame with the names of the states as first column 
   #     and the equivalent population
   #     size in the second column.
   #   h: the house size as variable.
+  #   q: the quota. Choose: 0 for Hare Quota(Default)
+  #                         1 for Droop Quota
+  #                         2 for Hagenbach-Bischoff Quota
+  #                         3 for Imperiali Quota
   #
   # Returns:
   #   A DataFrame containing all the Information 
   #   and the final result in the last column.
   
   psum <- sum(p[, 2])
-  a <- psum / h #Hare Quota! Needs to be chooseable!
+  
+  if(q == 0){
+    #Hare Quota(Default):
+    a <- psum / h 
+    
+  } else if(q == 1){
+    #Droop Quota
+    a <- 1 + psum/(h + 1)
+    
+  } else if(q == 2){#
+    #Hagenbach-Bischoff Quota
+    a <-  psum/(h + 1)
+    
+  } else if(q == 3){
+    #Imperiali Quota
+    a <- psum/(h + 2)
+    
+  } else {
+    # Exception!
+  }
+  
   
   p["share"] <- array(0, length(p[,1]))
   p["share.floor"] <- array(0, length(p[,1]))
