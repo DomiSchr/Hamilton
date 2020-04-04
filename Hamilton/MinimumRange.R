77c50888aca1be85f4343321f1d381122ad89de5
DomiSchr <domischroeder@gmx.de>
2020-04-04 12:30:27 +0200

    MinimumRange.R weiter

--- Hamilton/MinimumRange.R
+++ Hamilton/MinimumRange.R
@@ -1,7 +1,25 @@
-MinimumRange <- function(p, h){
+MinimumRange <- function(p, a, h){
+  
+  data <- data.frame("population" = p, "allotment" = a)
+  data["avg"] <- array(0, length(data[1,]))
+  
+  for(i in 1:length(data[, 1])){
+    data[i, "avg"] <- p[i] / a[i]
+  }
+  
+  #Caclulate the maximum disparty:
+  max.disparty <- 0
+  for(i in 1:length(data[, 1])){
+    for(j in 1:length(data[, 1])){
+      tmp <-  data[i, "avg"] /data[j,"avg"]
+      if(tmp > max.disparty){
+        max.disparty <- tmp
+      }
+    }
+  }
+    
+  ranks <- order(data[, "avg"])
   
   
-  
-  ranks <- order(p, decreasing = TRUE)
-  return(ranks)
+  return(max.disparty)
 }
