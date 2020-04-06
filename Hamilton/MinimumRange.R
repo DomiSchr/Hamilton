43b03f304df639cccfa56d17a16b625a76bc0e5a
DomiSchr <domischroeder@gmx.de>
2020-04-06 15:04:30 +0200

    MinimumRange.R weiter, keine Chance...

--- Hamilton/MinimumRange.R
+++ Hamilton/MinimumRange.R
@@ -1,42 +1,73 @@
-MinimumRange <- function(p, a, h){
-  
+MinimumRange <- function(p, a, h) {
   data <- data.frame("population" = p, "allotment" = a)
-  data["avg"] <- array(0, length(data[1,]))
+  data["avg"] <- array(0, length(data[1, ]))
   
-  for(i in 1:length(data[, 1])){
+  for (i in 1:length(data[, 1])) {
     data[i, "avg"] <- p[i] / a[i]
   }
   
+  
   max1 <- MaxDisparty(data)
+  
+  #Optimization by adding one seat the the state with the largest average consituency size:
+  #TODO: Repeat this process until largest average consituency can't be optimized...
   ranks <- order(data[, "avg"])
   
   data[ranks[1], "allotment"]  <- data[ranks[1], "allotment"] + 1
   
   max2 <- 0
-  tmp <-0
-
-  #Danach wieder eins draufrechnen, damit es insgesamt stimmt, so ziehe ich bei jeder Runde einen Sitz ab!  
-  for(i in 1:length(data[, 1]) - 1){
-    if(i != ranks[1]){
+  bool <- 0
+  
+  for (i in 1:length(data[, 1]) - 1) {
+    if (i != ranks[1]) {
       data[i, "allotment"] <- data[i, "allotment"] - 1
-      tmp <- MaxDisparty(data)
-      if(tmp < max2){
-        max2 <- tmp
+      max2 <- MaxDisparty(data)
+      if (max1 < max2) {
+        bool <- 1
+        break
       }
+      data[i, "allotment"] <- data[i, "allotment"] + 1
     }
   }
-    
-  max2 <- MaxDisparty(data)
-  if(max)
-  return()
+  
+  if(bool == 0){
+    return(data)
+  }
+  
+  #Optimization by adding one seat the the state with the largest average consituency size:
+  ranks <- order(data[, "avg"])
+  
+  data[ranks[1], "allotment"]  <- data[ranks[1], "allotment"] + 1
+  
+  max2 <- 0
+  bool <- 0
+  
+  for (i in 1:length(data[, 1]) - 1) {
+    if (i != ranks[1]) {
+      data[i, "allotment"] <- data[i, "allotment"] - 1
+      max2 <- MaxDisparty(data)
+      if (max1 < max2) {
+        bool <- 1
+        break
+      }
+      data[i, "allotment"] <- data[i, "allotment"] + 1
+    }
+  }
+  
+  if(bool == 0){
+    return(data)
+  }
+  
+  
+
 }
 
-MaxDisparty <- function(data){
+MaxDisparty <- function(data) {
   max.disparty <- 0
-  for(i in 1:length(data[, 1])){
-    for(j in 1:length(data[, 1])){
-      tmp <-  data[i, "avg"] /data[j,"avg"]
-      if(tmp > max.disparty){
+  for (i in 1:length(data[, 1])) {
+    for (j in 1:length(data[, 1])) {
+      tmp <-  data[i, "avg"] / data[j, "avg"]
+      if (tmp > max.disparty) {
         max.disparty <- tmp
       }
     }
