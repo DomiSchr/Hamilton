7f1b9b2f0b281be7d8e4df81d880b6d15f8d9d79
DomiSchr <49571547+DomiSchr@users.noreply.github.com>
2020-04-14 22:49:15 +0200

    MinimumRange.R weiter, geht aber noch net!

--- Hamilton/MinimumRange.R
+++ Hamilton/MinimumRange.R
@@ -2,61 +2,104 @@ MinimumRange <- function(p, a) {
   data <- data.frame("population" = p, "allotment" = a)
   data["avg"] <- array(0, length(data[, 1]))
   
-  print(data)
-  
-  for (i in 1:length(data[, 1])) {
-    data[i, "avg"] <- p[i] / a[i]
-  }
-  
-  max1 <- MaxDisparty(data)
-  
-  #Optimization by adding one seat the the state with the largest average consituency size:
-  #TODO: Repeat this process until largest average consituency can't be optimized...
-  ranks <- order(data[, "avg"])
-  
-  #Wiederholtes addieren, bis es nichtmehr kleiner wird
-  
-  max3 <- .Machine$integer.max
-  
-  while (max1 < max3) {
-    max3 <- max1
+  for (count in 0:(ceiling(length(data[, 1]) / 2))) {
+    #_________________________________________________________________________________________
+    #Biggest aj Value
+    max1 <- MaxDisparty(data)
+    #Optimization by adding one seat the the state with the largest average consituency size:
+    #TODO: Repeat this process until largest average consituency can't be optimized...
+    data <- CalcAvg(data)
+    ranks <- order(data[, "avg"])
     
-    data[ranks[length(ranks)], "allotment"]  <- data[ranks[length(ranks)], "allotment"] + 1
+    #Wiederholtes addieren, bis es nichtmehr kleiner wird
     
-    max2 <- 0
-    bool <- 0
+    max3 <- .Machine$integer.max
     
-    #Achtung: Hier ist die Stelle hardcoded
-    #Bei der ersten kleineren disparty wird abgebrochen!
-    #Frage: Bleibt man bei mehrfachem durchlaufen hier bei der ursprünglichen Ordnung?
-    for (i in 1:length(data[1, ]))  {
-      if (i != ranks[length(ranks)]) {
-        data[i, "allotment"] <- data[i, "allotment"] - 1
-        max2 <- MaxDisparty(data)
-        print("max2",  max2)
-        print("max1",  max1)
-        if (max2 >= max1) {
-          data[i, "allotment"] <- data[i, "allotment"] + 1
-          
-        } else {
-          max1 <- max2
-          bool <- bool + 1
-          break
+    #C zum testen
+    c <- 0
+    while (c < 5) {
+      max3 <- max1
+      data[ranks[length(ranks) - count], "allotment"]  <- data[ranks[length(ranks) - count], "allotment"] + 1
+      
+      max2 <- 0
+      bool <- 0
+      
+      #Bei der ersten kleineren disparty wird abgebrochen!
+      for (i in 1:length(data[,1]))  {
+        if (i != ranks[length(ranks)] - count) {
+          data[i, "allotment"] <- data[i, "allotment"] - 1
+          print("for")
+          max2 <- MaxDisparty(data)
+          if (max2 >= max1) {
+            data[i, "allotment"] <- data[i, "allotment"] + 1
+            
+          } else {
+            max1 <- max2
+            bool <- bool + 1
+            print("else")
+            break
+          }
         }
       }
+      if (bool == 0) {
+        data[ranks[length(ranks) - count], "allotment"]  <- data[ranks[length(ranks) - count], "allotment"] - 1
+      }
+      c <- c + 1
     }
-    if(bool == 0){
-      data[ranks[length(ranks)], "allotment"]  <- data[ranks[length(ranks)], "allotment"] - 1
+    
+    
+    #_________________________________________________________________________
+    #Smallest rj Value
+    max1 <- MaxDisparty(data)
+    
+    #Optimization by adding one seat the the state with the largest average consituency size:
+    #TODO: Repeat this process until largest average consituency can't be optimized...
+    data <- CalcAvg(data)
+    ranks <- order(data[, "avg"])
+    
+    #Wiederholtes addieren, bis es nichtmehr kleiner wird
+    
+    max3 <- .Machine$integer.max
+    
+    #C zum testen
+    c <-0 
+    while (c < 5) {
+      max3 <- max1
+      data[ranks[1 + count], "allotment"]  <- data[ranks[1 + count], "allotment"] + 1
+      
+      max2 <- 0
+      bool <- 0
+      
+      #Bei der ersten kleineren disparty wird abgebrochen!
+      for (i in 1:length(data[,1]))  {
+        if (i != ranks[1 + count]) {
+          data[i, "allotment"] <- data[i, "allotment"] - 1
+          max2 <- MaxDisparty(data)
+          if (max2 >= max1) {
+            data[i, "allotment"] <- data[i, "allotment"] + 1
+            
+          } else {
+            max1 <- max2
+            bool <- bool + 1
+            print("else")
+            break
+          }
+        }
+      }
+      if (bool == 0) {
+        data[ranks[1 + count], "allotment"]  <- data[ranks[1 + count], "allotment"] - 1
+      }
+      c <- c + 1
     }
   }
   
   return(data)
-
+  
 }
 
 #Kann man evtl. optimieren!
 # Größtes und kleinstes Element raussuchen und nur damit rechnen!
-MaxDisparty <- function(data) {
+MaxDisparty1 <- function(data) {
   max.disparty <- 0
   for (i in 1:length(data[, 1])) {
     for (j in 1:length(data[, 1])) {
@@ -68,3 +111,17 @@ MaxDisparty <- function(data) {
   }
   return(max.disparty)
 }
+
+MaxDisparty <- function(data){
+  data <- CalcAvg(data)
+  max <- max(data[, "avg"])
+  min <- min(data[, "avg"])
+  return(abs((max/min)-1))
+}
+
+CalcAvg <- function(data){
+  for (i in 1:length(data[, 1])) {
+    data[i, "avg"] <- data[i, "population"] / data[i, "allotment"]
+  }
+  return(data)
+} 
